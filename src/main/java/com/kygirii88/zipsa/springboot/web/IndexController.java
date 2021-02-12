801f33d1bb8f234aa2e469d6f19f2e6bfe1dc8b5
kygirii88 <kygirii88@gmail.com>
2021-02-13 01:09:49 +0900

    aws test

--- src/main/java/com/kygirii88/zipsa/springboot/web/IndexController.java
+++ src/main/java/com/kygirii88/zipsa/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.kygirii88.zipsa.springboot.web;
 
+import com.kygirii88.zipsa.springboot.config.auth.LoginUser;
+import com.kygirii88.zipsa.springboot.config.auth.dto.SessionUser;
 import com.kygirii88.zipsa.springboot.service.posts.PostsService;
 import com.kygirii88.zipsa.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
