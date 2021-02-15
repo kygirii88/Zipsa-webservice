ad0896091fd5d6b99281aadd4402bf2abf374730
kygirii88 <kygirii88@gmail.com>
2021-02-15 23:23:33 +0900

    프로젝트 설정

--- src/main/java/com/kygirii88/zipsa/springboot/web/HelloController.java
+++ src/main/java/com/kygirii88/zipsa/springboot/web/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "hello";
     }
 
     @GetMapping("/hello/dto")
